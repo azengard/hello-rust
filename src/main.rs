e6ffb71aea0608ef4a711d0a9786d79bcb5f0709
Carlos Henrique <azengard@users.noreply.github.com>
2017-10-10 11:02:09 -0300

    structs and functions with rust

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,34 @@ extern crate rand;
 fn main() { 
     let x = rand::random::<u8>();
     println!("rust é {}", x);
+
+    let y = add_num(1,2);
+    println!("Result {}", y);
+
+    let jose = Person::new(String::from("José"), 42);
+    info(jose);
 }
+
+fn add_num(x:u8, y:u8)->u8{
+    x+y
+}
+
+fn info(p:Person) {
+    println!("{} tem {} anos", p.name, p.age)
+}
+
+struct Person {
+    name: String,
+    age: u8
+}
+
+impl Person {
+    fn new(name:String, age:u8)->Person{
+        Person{name, age}
+    }
+}
+
+
+struct Nil; // unit struct
+
+struct Pair(i32, f32); //tuple struct
