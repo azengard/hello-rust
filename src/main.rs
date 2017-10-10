fb28938448574a462c3d74db4e0f81699dc5d0da
Carlos Henrique <azengard@users.noreply.github.com>
2017-10-10 11:26:15 -0300

    tipos imutaveis

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 extern crate rand;
 
 fn main() { 
+    // using random
     let x = rand::random::<u8>();
     println!("rust é {}", x);
 
+    // using function add_num
     let y = add_num(1,2);
     println!("Result {}", y);
 
+    // using function person
     let jose = Person::new(String::from("José"), 42);
     info(jose);
+
+    // using function print
+    let z = String::from("Rust !");
+    print(&z);
+    print(&z);
+
+    // using tipos multaveis
+    let mut vector:Vec<u8> = vec![1,2,3];
+    vector.push(4);
+    println!("Tipo multavel {:?}", vector)    
+}
+
+fn print(s:&String) {
+    println!("Func Print {}", s)
 }
 
 fn add_num(x:u8, y:u8)->u8{
